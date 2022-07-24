d6c835facedd13f5f8c95d69da4443898db4b06f
Alex Novikov <alexndr.novikov@gmail.com>
2022-07-25 00:42:52 +0400

    [feature] add for loop implementation

---
+++ internal/for/for.go
@@ -0,0 +1,33 @@
+package _for
+
+import "fmt"
+
+func For() {
+	fmt.Println("For package output:")
+	fmt.Println("Only condition loop from 1 to 5:")
+	i := 1
+	for i <= 5 {
+		fmt.Println(i)
+		i++
+
+	}
+	fmt.Println("Init;Condition;Loop for:")
+	for i := 1; i < 10; i++ {
+		fmt.Println(i)
+	}
+	fmt.Println("Infinite loop break && continue example:")
+
+	counter := 0
+	for {
+		if counter < 10 {
+			counter++
+			fmt.Println("Increment counter and continue:", counter)
+			continue
+		} else {
+			fmt.Println("Counter increment finished, break")
+			break
+		}
+	}
+
+	fmt.Println("")
+}
