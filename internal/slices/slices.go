7f3a1c51858368915d4c06e64862d9a416e5fdf1
Alex Novikov <anovikov@wowworks.ru>
2022-07-27 17:44:12 +0400

    [feature] implement slices example

---
+++ internal/slices/slices.go
@@ -0,0 +1,44 @@
+package slices
+
+import "fmt"
+
+func Slices() {
+	fmt.Println("Slices package output:")
+
+	fmt.Println("make slice length 3:")
+	s := make([]string, 3)
+	s[0] = "one"
+	s[1] = "two"
+	s[2] = "three"
+	fmt.Println(s)
+	fmt.Println(s[1])
+	fmt.Println(len(s))
+
+	s = append(s, "four")
+	s = append(s, "five", "six")
+	fmt.Println("Appended:", s)
+
+	c := make([]string, len(s))
+	copy(c, s)
+	s = append(s, "seven")
+	fmt.Println("Copy", c)
+	fmt.Println("Original", s)
+
+	fmt.Println("slice 2:5", s[2:5])
+	fmt.Println("slice :2", s[:2])
+	fmt.Println("slice 2:", s[2:])
+
+	shortSyntaxSlice := []int{1, 2, 3}
+	fmt.Println(shortSyntaxSlice)
+
+	twoDimArray := make([][]int, 3)
+	fmt.Println(twoDimArray)
+	for i := 0; i < 3; i++ {
+		twoDimArray[i] = make([]int, i+1)
+		for j := 0; j < len(twoDimArray[i]); j++ {
+			twoDimArray[i][j] = i + j
+		}
+	}
+	fmt.Println(twoDimArray)
+	fmt.Println("")
+}
