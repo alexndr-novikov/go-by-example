6131b1cf799fb0cb3c9afa57bc84d8d19e00ff9c
Alex Novikov <anovikov@wowworks.ru>
2022-07-27 14:23:53 +0400

    [feature] implement arrays topic

---
+++ internal/array/array.go
@@ -0,0 +1,29 @@
+package array
+
+import "fmt"
+
+func Array() {
+	fmt.Println("Array package output:")
+
+	fmt.Println("Zero value array example:")
+	a := [5]int{}
+	fmt.Println("Emp:", a)
+
+	a[4] = 100
+	fmt.Println("Updated:", a)
+	fmt.Println("Last element:", a[4])
+	fmt.Println("len:", len(a))
+
+	b := [5]int{1, 2, 3, 4}
+	fmt.Println("New arr:", b)
+
+	twoDim := [2][3]int{}
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 3; j++ {
+			twoDim[i][j] = i + j
+		}
+	}
+	fmt.Println("Tow dim array:", twoDim)
+
+	fmt.Println("")
+}
