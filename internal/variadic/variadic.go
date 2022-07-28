cc563244be287c6c984618e8212cb13a0a04440f
Alex Novikov <anovikov@wowworks.ru>
2022-07-28 17:56:54 +0400

    [feature] variadic argiments, closures

---
+++ internal/variadic/variadic.go
@@ -0,0 +1,22 @@
+package variadic
+
+import "fmt"
+
+func Variadic() {
+	fmt.Println("Variadic package output:")
+
+	sum(1, 10, 100, 1000)
+	fmt.Println("")
+
+	nums := []int{1, 100, 10000}
+	sum(nums...)
+}
+
+func sum(nums ...int) {
+	fmt.Println(nums)
+	total := 0
+	for _, num := range nums {
+		total += num
+	}
+	fmt.Println(total)
+}
