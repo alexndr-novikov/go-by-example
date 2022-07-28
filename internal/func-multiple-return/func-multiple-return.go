0603ad03aa76511d846b79885d1310f1220a1698
Alex Novikov <anovikov@wowworks.ru>
2022-07-28 10:57:33 +0400

    [feature] implement func multiple return values

---
+++ internal/func-multiple-return/func-multiple-return.go
@@ -0,0 +1,20 @@
+package func_multiple_return
+
+import "fmt"
+
+func FuncMultipleReturnValues() {
+	fmt.Println("FuncMultipleReturnValues package output:")
+
+	a := 1
+	b := 2
+	sum, newA, _ := sumStoreOriginal(a, b)
+	fmt.Println("Sum:", sum)
+	fmt.Println("New A:", newA)
+	fmt.Println("Equal?:", a == newA)
+	fmt.Println("")
+	fmt.Println("")
+}
+
+func sumStoreOriginal(a, b int) (int, int, int) {
+	return a + b, a, b
+}
